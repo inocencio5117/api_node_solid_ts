8a5310081e9aa9f985c152200155c8767f394b89
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2021-12-27 10:36:20 -0300

    Trying to fix default connection error

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,8 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import {
+  createConnection,
+  getConnectionManager,
+  getConnectionOptions,
+} from "typeorm";
 
 interface IOptions {
   host: string;
@@ -6,8 +10,26 @@ interface IOptions {
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
-  newOptions.host = "database";
+  newOptions.host = "database_ignite";
+
   createConnection({
     ...options,
   });
 });
+
+// const connectionManager = getConnectionManager();
+
+// if(!connectionManager.has("default")){
+//     // ? load connection options from ormconfig or environment
+//     const connectionOptions = await getConnectionOptions();
+//     connectionManager.create(connectionOptions);
+// }
+
+// // ? connect to the database
+// try {
+//     db = connectionManager.get();
+//     await db.connect();
+// } catch (error) {
+//     console.log(error);
+//     return;
+// }
