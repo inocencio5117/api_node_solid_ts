f3c5a5a971d13142fec6f7222facacc700d63394
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-01-13 10:48:20 -0300

    refactoring refresh_token authentication

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "1f95c04fd7c5c1f3c36848ff70da7242" as string,
-  expires_in_token: "15m",
+  expires_in_token: "15m" as string,
   secret_refresh_token: "e57941dbe1246fe97a4ffc16e85b5df9" as string,
   expires_in_refresh_token: "30d" as string,
   expires_refresh_token_days: 30 as number,
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,4 @@
 import auth from "@config/auth";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { UsersTokenRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokenRepository";
 import { AppError } from "@shared/errors/AppError";
 import { NextFunction, request, Request, Response } from "express";
@@ -15,7 +14,6 @@ export async function ensureAuthenticated(
   next: NextFunction
 ): Promise<void> {
   const authHeader = req.headers.authorization;
-  const usersTokenRepository = new UsersTokenRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -24,20 +22,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
-    console.log(user_id);
-
-    const user = usersTokenRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) {
-      throw new AppError("User does not exist", 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
