bdda38257b44ede29fdd3d31c8346c96250cdc61
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-01-13 15:47:23 -0300

    creating avatar access url

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,5 +1,6 @@
 import { CarImageRepository } from "@modules/cars/infra/typeorm/repositories/CarImageRepository";
 import { ICarImageRepository } from "@modules/cars/repositories/ICarImageRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -11,12 +12,16 @@ interface IRequest {
 class UploadCarImageUseCase {
   constructor(
     @inject("CarImageRepository")
-    private carImageRepository: ICarImageRepository
+    private carImageRepository: ICarImageRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,6 +12,7 @@ import createConnection from "@shared/infra/typeorm";
 
 import "../../container";
 import { AppError } from "@shared/errors/AppError";
+import upload from "@config/upload";
 
 createConnection();
 const app = express();
@@ -20,6 +21,9 @@ app.use(express.json());
 app.use(router);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
