51f7c1728e9325b563c0af5c4039d44451274c4e
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-01-15 21:48:23 -0300

    error logs in sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,9 @@ import express, { NextFunction, Response, Request } from "express";
 import swaggerUi from "swagger-ui-express";
 import "express-async-errors";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
 
@@ -20,11 +23,41 @@ import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 createConnection();
 const app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(rateLimiter);
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      if (error.status === 404 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
